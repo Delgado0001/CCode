97656c6db5e9da0e2f9a7399fa6b320948492a2b
Delgado0001 <43787785+Delgado0001@users.noreply.github.com>
2019-02-04 10:21:17 -0800

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,15 @@
+// hello.c
+// compile gcc hello.c -o hello.o
+// run ./hello.o
+
+#include <studio.h>
+
+int main(){
+	printf("Hello, World!");
+	printf("*********************\n");
+	printf(" ddd  d     d   ggg  \n");
+	printf("d      d d d   g     \n");
+	printf(" ddd    d d     ggg  \n");
+	printf("*********************\n");
+	return 0;
+}
