225c66a03bceaea858262f1986032e88f24c1922
icebowl <craigcoleman.cs@gmail.com>
2019-05-14 10:02:45 -0700

    AA

---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+// main function to call above defined funtion.
+int main () {
+   cout<<"		Hello World"<<endl;
+   cout<<"America Will Once More Be Great Again"<<endl;
+   cout<<"YES WE CAN!!! For 'I HAVE A DREAM' "<<endl;
+}
